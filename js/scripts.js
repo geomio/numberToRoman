ae8b95da3007e7df69eb2aca2eaa2e476ad738d4
geomio <geoff.millner22@gmail.com>
2021-05-25 11:27:21 -0700

    made js code and html to convert and display roman numerals from numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,40 @@
-let userNumber = 1992;
 
-let convertToRoman = function(inputNum) {
-  console.log(typeof inputNum);
-  if (typeof inputNum !== "number") {
-    console.log('this is not a inputNum')
-  }
-  let numberValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  let letterValue = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
 
-  let roman = "";
-  if ((userNumber >= 4000) || (userNumber <= 0)) {
-    console.log('pick a number between 1-3999');
-  } else {
-   for (let index = 0; index < numberValue.length; index++) {
-    while (numberValue[index] <= inputNum) {
-      roman += letterValue[index];
-      inputNum -= numberValue[index];
-    }
-  }
-  console.log(roman)
-  return roman;
-  }; 
-};
-convertToRoman(userNumber);
 
-//followed along partially from https://dev.to/daniel6/convert-to-number-roman-with-javascript-23ol 
+
+//followed along partially from  
 // also help from daniel
 // and w3schools.com 
+
+$(document).ready(function () {
+  $("form#numbers").submit(function(event){
+    let userNumber= document.getElementById("numToJs").value;
+    parseInt(userNumber);
+    let convertToRoman = function(inputNum) {
+      console.log(typeof inputNum);
+      if (typeof inputNum !== "number") {
+        console.log('this is not a inputNum')
+      }
+      let numberValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+      let letterValue = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+    
+      let roman = "";
+      if ((userNumber >= 4000) || (userNumber <= 0)) {
+        alert('pick a number between 1-3999');
+      } else {
+       for (let index = 0; index < numberValue.length; index++) {
+        while (numberValue[index] <= inputNum) {
+          roman += letterValue[index];
+          inputNum -= numberValue[index];
+        }
+      }
+      return roman;
+      }; 
+    };
+    convertToRoman(userNumber);
+    $("#romNum").text(`${convertToRoman(userNumber)}`);
+    
+    $("#output").show();
+    event.preventDefault();
+  });
+});
---
+++ readme.md
@@ -0,0 +1,71 @@
+# 
+
+  
+
+#### Javascript html number to roman numeral converter.
+
+Will convert numbers between 1-3999 to roman numerals.
+
+  
+
+#### By Geoff Millner
+
+  
+
+## Description
+
+a basic page with form area and input for numbers and a hidden area that will display answer. als it will alert if the wrong numbers below 1 or over 3999 are used
+  
+  
+
+# Setup/Installation Requirements
+
+1. have internet connection
+2. have browser such as google
+3. open link via git hub here
+
+or
+
+1. go to 
+2. copy link
+3. in terminal navigate to desired folder
+4. type in terminal: git clone
+5. open index.html with browser
+
+must have computer with internet connection.
+
+# Known Bugs
+
+
+mobile layout may be rather condensed.
+
+
+# Support and contact details
+
+  
+
+_{Check my github for info.}_
+
+  
+
+# Technologies Used
+
+  
+
+_Html5 bootstrap.css and .css stylesheet, made in Visual Studio Code._
+
+  
+
+### License
+
+  
+cc0 feel free to use. 2021 * by_geoff millner_*
+
+## credits
+
+thanks for theses sources where I learned about roman numerals
+
+1.https://www.w3resource.com/javascript-exercises/javascript-math-exercise-21.php
+2.https://dev.to/daniel6/convert-to-number-roman-with-javascript-23ol
+3.Daniel of Fidgeitech/Fidigetech lessons
+4.And wikipedia
